41406554137bb830b5e50cf30fbc2ac1a2b832a8
Caleb L <caleb.lenoir@gmail.com>
2013-03-09 15:44:18 -0500

    put data together as csv

--- js/liquid.js
+++ js/liquid.js
@@ -1,7 +1,7 @@
 Parse.User.logIn("calebl", "password", {
   success: function(user) {
-	addContainer();
-	getForms();
+	// addContainer();
+	// getForms();
 
   },
   error: function(user, error) {
@@ -11,41 +11,26 @@ Parse.User.logIn("calebl", "password", {
   }
 });
 
-function addContainer() {
-	for(i=0; i<9; i++) {
-		$("#addFile").append("<div class='span1'><img src='img/glyphicons_029_notes_2.png' /></div>");
-	}
-	
-};
+$(document).ready(function(){
+
+});
+
+var Form = Parse.Object.extend('forms');
+var FormCollection = Parse.Collection.extend({
+	model: Form,
+	query: (new Parse.Query(Form)).equalTo("user", Parse.User.current()),
+	comparator: function(object){return object.get('createdAt');}
+});
+var form_collection = new FormCollection();
 
 function getForms(){
+
 	var currentUser = Parse.User.current();
 	if (currentUser) {
 	    // do stuff with the user
-
-	    var Form = Parse.Object.extend('forms');
-	 //    var query = new Parse.Query(Form);
-		// query.equalTo("user", currentUser);
-		// query.find({
-		//   success: function(results) {
-		//     // alert("Successfully retrieved " + results.length + " scores.");
-		//     addFormContainers(results);
-		//   },
-		//   error: function(error) {
-		//     alert("Error: " + error.code + " " + error.message);
-		//   }
-		// });
-
-		var FormCollection = Parse.Collection.extend({
-		  model: Form,
-		  query: (new Parse.Query(this.model)).equalTo("user", currentUser)
-		});
-		var form_collection = new FormCollection();
 		form_collection.fetch({
 			success: function(collection) {
-				collection.each(function(object) {
-					console.warn(object);
-				});
+				addFormContainers(collection);
 			},
 			error: function(collection, error) {
 			// The collection could not be retrieved.
@@ -56,8 +41,26 @@ function getForms(){
 	}
 }
 
-function addFormContainers(results){
-	_(results).each(function(result){
-		// var form_name = result.get('')
+function addFormContainers(collection){
+	collection.forEach(function(form){
+		var $form_el = $("<li class='span1'><img src='img/glyphicons_029_notes_2.png' /></li>");
+		$form_el.data({
+			headers: form.get('headers'),
+			contents: form.get('contents'), 
+			created_at: form.get('createdAt')});
+		$("#addFile").append($form_el);
 	});
+}
+
+function downloadCSV(){
+	var headers,
+		contents = [];
+	$('li.ui-selected').each(function(){
+		headers = $(this).data('headers');
+		contents.push($(this).data('contents'));
+	})
+	var output = headers + '\n' + contents.join('\n');
+	var uri = 'data:application/csv;charset=UTF-8,' + encodeURIComponent(output);
+  	$dl_el = $('<a>').attr('href',uri).attr('download','form_data.csv').addClass('dl_link').html('download');
+  	$('#addFile').append($dl_el);
 }
