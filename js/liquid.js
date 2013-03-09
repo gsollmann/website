51c7276714dbfbf1f5aa6628c4130ef524349739
Caleb L <caleb.lenoir@gmail.com>
2013-03-09 13:32:31 -0500

    adding jquery ui

--- js/liquid.js
+++ js/liquid.js
@@ -1,9 +1,12 @@
 Parse.User.logIn("calebl", "password", {
   success: function(user) {
-	addContainer()
+	addContainer();
+	getForms();
 
   },
   error: function(user, error) {
+  	//TODO: show the login page
+
     // The login failed. Check error to see why.
   }
 });
@@ -14,3 +17,47 @@ function addContainer() {
 	}
 	
 };
+
+function getForms(){
+	var currentUser = Parse.User.current();
+	if (currentUser) {
+	    // do stuff with the user
+
+	    var Form = Parse.Object.extend('forms');
+	 //    var query = new Parse.Query(Form);
+		// query.equalTo("user", currentUser);
+		// query.find({
+		//   success: function(results) {
+		//     // alert("Successfully retrieved " + results.length + " scores.");
+		//     addFormContainers(results);
+		//   },
+		//   error: function(error) {
+		//     alert("Error: " + error.code + " " + error.message);
+		//   }
+		// });
+
+		var FormCollection = Parse.Collection.extend({
+		  model: Form,
+		  query: (new Parse.Query(this.model)).equalTo("user", currentUser)
+		});
+		var form_collection = new FormCollection();
+		form_collection.fetch({
+			success: function(collection) {
+				collection.each(function(object) {
+					console.warn(object);
+				});
+			},
+			error: function(collection, error) {
+			// The collection could not be retrieved.
+			}
+		});
+	} else {
+	    // show the signup or login page
+	}
+}
+
+function addFormContainers(results){
+	_(results).each(function(result){
+		// var form_name = result.get('')
+	});
+}
