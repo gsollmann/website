7f5f2684127dc6540d13a6ffeb1b942cc17dc1c4
Jacob <jacob@liquid-innovation.com>
2013-03-09 11:57:42 -0500

    Updated containers

--- js/liquid.js
+++ js/liquid.js
@@ -1,8 +1,16 @@
 Parse.User.logIn("calebl", "password", {
   success: function(user) {
-    alert('logged in as calebl')
+	addContainer()
+
   },
   error: function(user, error) {
     // The login failed. Check error to see why.
   }
 });
+
+function addContainer() {
+	for(i=0; i<9; i++) {
+		$("#addFile").append("<div class='span1'><img src='img/glyphicons_029_notes_2.png' /></div>");
+	}
+	
+};
